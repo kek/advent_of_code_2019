ac0b53fa08301ea41acd679a70c8f2541532a7a7
Karl Eklund <localpart@gmail.com>
2019-12-04 21:01:04 +0100

    day 1 part 2

--- lib/advent_of_code2019/day1.ex
+++ lib/advent_of_code2019/day1.ex
@@ -1,5 +1,25 @@
 defmodule AdventOfCode2019.Day1 do
   def fuel_required(mass) do
-    floor(mass / 3) - 2
+    case floor(mass / 3) - 2 do
+      n when n < 0 -> 0
+      n -> n
+    end
+  end
+
+  # So, for each module mass, calculate its fuel and add it to the total. Then,
+  # treat the fuel amount you just calculated as the input mass and repeat the
+  # process, continuing until a fuel requirement is zero or negative.
+  def more_fuel_required(0), do: 0
+  def more_fuel_required(mass) when mass < 0, do: 0
+
+  def more_fuel_required(mass) do
+    fuel_for_mass =
+      case floor(mass / 3) - 2 do
+        n when n < 0 -> 0
+        n -> n
+      end
+
+    fuel_for_fuel = more_fuel_required(fuel_for_mass)
+    fuel_for_mass + fuel_for_fuel
   end
 end
--- test/day1_test.exs
+++ test/day1_test.exs
@@ -2,39 +2,6 @@ defmodule AdventOfCode2019.Day1.Test do
   use ExUnit.Case
 
   alias AdventOfCode2019.Day1
-  # --- Day 1: The Tyranny of the Rocket Equation ---
-
-  # Santa has become stranded at the edge of the Solar System while delivering
-  # presents to other planets! To accurately calculate his position in space,
-  # safely align his warp drive, and return to Earth in time to save Christmas,
-  # he needs you to bring him measurements from fifty stars.
-
-  # Collect stars by solving puzzles. Two puzzles will be made available on each
-  # day in the Advent calendar; the second puzzle is unlocked when you complete
-  # the first. Each puzzle grants one star. Good luck!
-
-  # The Elves quickly load you into a spacecraft and prepare to launch.
-
-  # At the first Go / No Go poll, every Elf is Go until the Fuel Counter-Upper.
-  # They haven't determined the amount of fuel required yet.
-
-  # Fuel required to launch a given module is based on its mass. Specifically,
-  # to find the fuel required for a module, take its mass, divide by three,
-  # round down, and subtract 2.
-
-  # For example:
-
-  # For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2.
-  # For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2.
-  # For a mass of 1969, the fuel required is 654.
-  # For a mass of 100756, the fuel required is 33583.
-
-  # The Fuel Counter-Upper needs to know the total fuel requirement. To find it,
-  # individually calculate the fuel needed for the mass of each module (your
-  # puzzle input), then add together all the fuel values.
-
-  # What is the sum of the fuel requirements for all of the modules on your
-  # spacecraft?
 
   test "specification" do
     assert Day1.fuel_required(12) == 2
@@ -52,5 +19,37 @@ defmodule AdventOfCode2019.Day1.Test do
       |> Enum.sum()
 
     assert result == 3_426_455
+  end
+
+  test "part 2" do
+    # A module of mass 14 requires 2 fuel. This fuel requires no further fuel (2
+    # divided by 3 and rounded down is 0, which would call for a negative fuel),
+    # so the total fuel required is still just 2.
+
+    assert Day1.more_fuel_required(14) == 2
+
+    # At first, a module of mass 1969 requires 654 fuel. Then, this fuel
+    # requires 216 more fuel (654 / 3 - 2). 216 then requires 70 more fuel,
+    # which requires 21 fuel, which requires 5 fuel, which requires no further
+    # fuel. So, the total fuel required for a module of mass 1969 is 654 + 216 +
+    # 70 + 21 + 5 = 966.
+
+    assert Day1.more_fuel_required(1969) == 966
+
+    # The fuel required by a module of mass 100756 and its fuel is: 33583 +
+    # 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346.
+
+    assert Day1.more_fuel_required(100_756) == 50346
+  end
+
+  test "part 2 solution" do
+    result =
+      File.read!("priv/day1_input.txt")
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.map(&Day1.more_fuel_required/1)
+      |> Enum.sum()
+
+    assert result == 5_136_807
   end
 end
